5f31b102506255d81cae9c86a15621153dbe2387
jmona789 <jmona789@gmail.com>
2016-02-06 11:22:37 -0500

    fixed port issue

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var PORT = 8090;
+var PORT = process.env.PORT || 8090;
 
 app.use("/static", express.static("public"));
 
@@ -13,6 +13,9 @@ app.get("/reg", function(req, res){
   res.sendFile(process.cwd() + "/register.html")
 });
 
+app.get("/dash", function(req, res){
+  res.sendFile(process.cwd() + "/dashboard.html")
+});
 
 app.listen(PORT, function(){
   console.log("listening on port %s", PORT);
